406498726692040fbdbb2c102e6a4cf3a14c5298
Adam <adam.and.math@gmail.com>
2016-06-02 14:39:20 +0200

    Added Spawner

---
+++ Project/Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class Spawner : MonoBehaviour
+{
+    public EnemyBase enemy;
+    public float spawnRate;
+    public string timerParameter = "Timer";
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetFloat(timerParameter, spawnRate);
+    }
+
+    void Update()
+    {
+        animator.SetFloat(timerParameter, animator.GetFloat(timerParameter) - Time.deltaTime);
+    }
+
+    public void Spawn()
+    {
+        animator.SetFloat(timerParameter, animator.GetFloat(timerParameter) + spawnRate);
+        EnemyBase spawn = Instantiate(enemy);
+        spawn.transform.position = transform.position;
+    }
+}
