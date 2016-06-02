8162aa5e87e1f9dc17d5173b0ea1189f42e1a94b
Adam <adam.and.math@gmail.com>
2016-06-02 21:10:04 +0200

    Added HUD prefab

---
+++ Project/Assets/Scripts/HUD.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Text score;
+    public Text lives;
+    public PlayerMovement player;
+    public PrisonerExit exit;
+
+    private string scoreFormat;
+    private string livesFormat;
+
+    void Start()
+    {
+        scoreFormat = score.text;
+        livesFormat = lives.text;
+    }
+
+    void Update()
+    {
+        score.text = string.Format(scoreFormat, player.score);
+        lives.text = string.Format(livesFormat, exit.health - exit.damage, exit.health);
+    }
+}
