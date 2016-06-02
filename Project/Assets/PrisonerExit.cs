3af3209718c934eedcde628bd088837e25936cfd
Allan <aldanv@live.dk>
2016-06-02 17:03:55 +0200

    Added exit, can handle score and damage/health

---
+++ Project/Assets/PrisonerExit.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrisonerExit : MonoBehaviour {
+
+    public GameObject player;
+
+    public int health;
+    public int damage;
+
+    public int score;
+    public int scorePenalty;
+
+
+	void OnTriggerEnter (Collider other) {
+        if (other.gameObject != player)
+        {
+            Destroy(other.gameObject);
+            //different foe, different damage
+            //health -= other.gameObject.damage;
+            //same damage for all
+            health -= damage;
+
+
+            score -= scorePenalty;
+        }
+	}
+}
