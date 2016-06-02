5b65ae9e7ef3640a8208637406814238d366731a
Adam <adam.and.math@gmail.com>
2016-06-03 00:09:46 +0200

    Shows your score before submit

---
+++ Project/Assets/Scripts/UIScore.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIScore : UIBehaviour
+{
+    private Text text;
+    private string format;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        format = text.text;
+    }
+
+    void Update()
+    {
+        text.text = string.Format(format, SaveFile.newScore);
+    }
+}
